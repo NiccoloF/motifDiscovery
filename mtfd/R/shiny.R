5acef736fdd850bba8655d8640b92f95ae60dff2
NiccoloF <niccolo.feresini@mail.polimi.it>
2024-07-31 21:07:05 +0200

    added error handling

--- mtfd/R/shiny.R
+++ mtfd/R/shiny.R
@@ -2,19 +2,45 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(shinyjs)
+library(shinyWidgets)
+library(shinybusy)
 
 ui <- fluidPage(
   useShinyjs(),  # Initialize shinyjs
+  useSweetAlert(),  # Initialize shinyWidgets
   
   tags$head(
     tags$style(HTML("
+      /* Your custom CSS */
       body {
         background-color: #001f3f;
-        color: white;
+        color: #ffffff;
+        font-family: 'Arial', sans-serif;
       }
       .well {
         background-color: #003366;
         color: white;
+        border: none;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
+        padding: 20px;
+      }
+      .btn-primary {
+        background-color: #007bff;
+        border: none;
+        padding: 6px 12px;  /* Increased padding for larger clickable area */
+        font-size: 18px;     /* Larger font size */
+        cursor: pointer;
+        border-radius: 8px;  /* Rounded corners */
+        transition: background-color 0.3s, transform 0.2s; /* Smooth transitions */
+      }
+      .btn-primary:hover {
+        background-color: #0056b3;
+        transform: scale(1.05);  /* Slightly increase button size on hover */
+      }
+      .btn-primary:active {
+        background-color: #004080;
+        transform: scale(1.02);  /* Slightly reduce button size on click */
       }
       #pagination_controls {
         display: flex;
@@ -23,19 +49,41 @@ ui <- fluidPage(
       }
       .pagination-buttons {
         margin: 0 10px;
+        background-color: #007bff;
+        border: none;
+        color: white;
+        padding: 6px 12px;  /* Increased padding for larger clickable area */
+        font-size: 18px;     /* Larger font size */
+        border-radius: 8px;  /* Rounded corners */
+        cursor: pointer;
+        transition: background-color 0.3s, transform 0.2s; /* Smooth transitions */
+      }
+      .pagination-buttons:disabled {
+        background-color: #cccccc;
+        cursor: not-allowed;
+      }
+      .pagination-buttons:hover:not(:disabled) {
+        background-color: #0056b3;
+        transform: scale(1.05);  /* Slightly increase button size on hover */
+      }
+      .pagination-buttons:active:not(:disabled) {
+        background-color: #004080;
+        transform: scale(1.02);  /* Slightly reduce button size on click */
       }
       .collapsible {
         cursor: pointer;
-        padding: 10px;
+        padding: 12px;
         background-color: #003366;
         border: none;
         color: white;
         text-align: left;
         outline: none;
-        font-size: 16px;
+        font-size: 18px;  /* Larger font size */
         width: 100%;
         display: block;
         margin-bottom: 5px;
+        border-radius: 8px;  /* Rounded corners */
+        transition: background-color 0.3s; /* Smooth transitions */
       }
       .active, .collapsible:hover {
         background-color: #00509e;
@@ -44,6 +92,8 @@ ui <- fluidPage(
         display: none;
         padding: 10px;
         background-color: #002244;
+        border-radius: 5px;
+        margin-bottom: 10px;
       }
       .plot-container {
         margin-bottom: 20px;
@@ -55,6 +105,7 @@ ui <- fluidPage(
       .input-row {
         display: flex;
         justify-content: space-between;
+        flex-wrap: wrap;
       }
       .input-group {
         flex: 1;
@@ -62,6 +113,11 @@ ui <- fluidPage(
       }
       .input-group:last-child {
         margin-right: 0;
+      }
+      #error_message {
+        color: red;
+        font-weight: bold;
+        margin-top: 20px;
       }
     ")),
     tags$script(HTML("
@@ -86,7 +142,8 @@ ui <- fluidPage(
              )
       )
     ),
-    downloadButton("downloadPdf", "Download PDF")
+    downloadButton("downloadPdf", "Download PDF"),
+    div(id = "error_message", "")
   ),
   
   fluidRow(
@@ -106,9 +163,9 @@ ui <- fluidPage(
              ),
              div(class = "input-row",
                  div(class = "input-group", numericInput("min_dist_motifs", "Minimum Distance Between Motifs (min_dist_motifs)", value = 30, min = 1)),
-                 div(class = "input-group", selectInput("distribution", "Coefficient Distribution", choices = c("unif", "norm"), selected = "unif"))
+                 div(class = "input-group", selectInput("distribution", "Coefficient Distribution", choices = c("unif", "beta"), selected = "unif"))
              ),
-             numericInput("numMotifs", "Number of Motifs", value = 5, min = 1),
+             numericInput("numMotifs", "Number of Motifs", value = 2, min = 1),
              uiOutput("motifInputs"),
              textInput("path", "Output Directory", value = tempdir())
            )
@@ -137,157 +194,171 @@ server <- function(input, output, session) {
   observeEvent(input$plotBtn, {
     req(input$path)
     
-    curve_details <- list(
-      N = input$N,
-      len = input$len,
-      norder = input$norder,
-      coeff_min = input$coeff_min,
-      coeff_max = input$coeff_max,
-      dist_knots = input$dist_knots,
-      min_dist_motifs = input$min_dist_motifs
-    )
+    # Show progress bar
+    show_modal_spinner(spin = "circle", text = "Generating plots, please wait...")
     
-    parse_weights <- function(weights_input) {
-      if (weights_input == "") {
-        return(NULL)
-      }
-      as.numeric(unlist(strsplit(weights_input, ",")))
-    }
-    
-    mot_details <- lapply(1:input$numMotifs, function(i) {
-      list(
-        len = input[[paste0("mot", i, "_len")]],
-        weights = parse_weights(input[[paste0("mot", i, "_weights")]]),
-        appearance = input[[paste0("mot", i, "_appearance")]]
+    tryCatch({
+      curve_details <- list(
+        N = input$N,
+        len = input$len,
+        norder = input$norder,
+        coeff_min = input$coeff_min,
+        coeff_max = input$coeff_max,
+        dist_knots = input$dist_knots,
+        min_dist_motifs = input$min_dist_motifs
       )
-    })
-    
-    distribution <- input$distribution
-    path <- input$path
-    
-    # Run the motifSimulationBuilder algorithm
-    builder <- motifSimulationBuilder(curve_details, mot_details, distribution)
-    
-    curves <- generateCurves(builder,0.1)
-    
-    output_file <- file.path(path, "plots.pdf")
-    
-    # Create the directory if it does not exist
-    if (!dir.exists(path)) {
-      dir.create(path)
-    }
-    
-    # Generate the plots
-    plots <- lapply(seq_along(curves), function(i) {
-      curve_data <- data.frame(
-        t = seq(0, curves[[i]]$basis$rangeval[2]),
-        x = eval.fd(seq(0, curves[[i]]$basis$rangeval[2]), curves[[i]], Lfdobj = 0)
-      )
-      names(curve_data) <- c("t","x")
       
-      motif_lines <- mapply(function(id_motif, pos_motif, instance) {
-        motif_t = seq((pos_motif - 1) * builder@dist_knots,
-                      (pos_motif - 1 + length(curves$coeff_motifs[[id_motif]]) - builder@norder + 1) * builder@dist_knots)
-        motif_x = eval.fd(motif_t, curves[[i]], Lfdobj = 0)
-        data.frame(t = motif_t, x = motif_x, motif_id = factor(paste(id_motif, instance, sep = "_")),
-                   initial_number = str_extract(as.character(id_motif), "^[^_]+"),
-                   xmin = (pos_motif - 1) * builder@dist_knots,
-                   xmax = (pos_motif - 1 + length(curves$coeff_motifs[[id_motif]]) - builder@norder + 1) * builder@dist_knots)
-      }, builder@motifs_in_curves[[i]]$motif_id, builder@motifs_in_curves[[i]]$starting_coeff_pos, seq_along(builder@motifs_in_curves[[i]]$motif_id), SIMPLIFY = FALSE)
-      
-      motif_data <- bind_rows(motif_lines)
-      names(motif_data) <- c("t","x","motif_id","initial_number","xmin","xmax")
-      
-      motif_colors <- c( "1" = "red", "2" = "green", "3" = "blue", "4" = "orange",
-                         "5" = "purple", "6" = "cyan", "7" = "magenta", "8" = "brown",
-                         "9" = "pink", "10" = "grey")
-      motif_colors <- rep(motif_colors,length.out = length(mot_details))
-      if(length(mot_details) > 10 )
-        attr(motif_colors,"names")[11:length(mot_details)] <- as.character(as.integer(attr(motif_colors,"names")[11:length(mot_details)]) + 10)
-      
-      if (nrow(motif_data) > 0) {
-        p <- ggplot() +
-          # Plot the main curve in black
-          geom_line(data = curve_data, aes(x = t, y = x), color = "black", size = 0.5) +
-          # Add shaded rectangles for motif positions with transparency
-          geom_rect(data = motif_data, aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf, fill = initial_number), alpha = 0.01) +
-          # Plot motifs with distinct colors
-          geom_line(data = motif_data, aes(x = t, y = x, color = factor(initial_number), group = motif_id), size = 1.5) + 
-          scale_color_manual(values = motif_colors) +
-          scale_fill_manual(values = motif_colors) +
-          labs(title = paste('Random curve', i), x = 't', y = 'x(t)') +
-          theme_minimal(base_size = 15) +
-          ylim(-20, 20) +
-          guides(color = guide_legend(title = "Motif ID"), fill = guide_legend(title = "Motif ID"))
+      parse_weights <- function(weights_input) {
+        if (weights_input == "") {
+          return(NULL)
+        }
+        as.numeric(unlist(strsplit(weights_input, ",")))
       }
-      p
-    })
-    
-    # Pagination settings
-    plots_per_page <- 2
-    num_pages <- ceiling(length(plots) / plots_per_page)
-    current_page <- reactiveVal(1)
-    
-    output$plots_ui <- renderUI({
-      page <- current_page()
-      start <- (page - 1) * plots_per_page + 1
-      end <- min(page * plots_per_page, length(plots))
       
-      plot_output_list <- lapply(start:end, function(i) {
-        plotname <- paste0("plot", i)
-        fluidRow(
-          column(
-            width = 12,
-            div(class = "plot-container",
-                div(class = "plot-title"),
-                plotOutput(outputId = plotname, height = "350px", width = "150%")
-            )
-          )
+      mot_details <- lapply(1:input$numMotifs, function(i) {
+        list(
+          len = input[[paste0("mot", i, "_len")]],
+          weights = parse_weights(input[[paste0("mot", i, "_weights")]]),
+          appearance = input[[paste0("mot", i, "_appearance")]]
         )
       })
       
-      do.call(tagList, plot_output_list)
-    })
-    
-    lapply(seq_along(plots), function(i) {
-      output[[paste0("plot", i)]] <- renderPlot({
-        plots[[i]]
-      })
-    })
-    
-    observe({
-      shinyjs::enable("prevPage")
-      shinyjs::enable("nextPage")
+      distribution <- input$distribution
+      path <- input$path
       
-      if (current_page() == 1) {
-        shinyjs::disable("prevPage")
+      # Run the motifSimulationBuilder algorithm
+      builder <- motifSimulationBuilder(curve_details, mot_details, distribution)
+      
+      curves <- generateCurves(builder,0.1)
+      
+      output_file <- file.path(path, "plots.pdf")
+      
+      # Create the directory if it does not exist
+      if (!dir.exists(path)) {
+        dir.create(path)
       }
       
-      if (current_page() == num_pages) {
-        shinyjs::disable("nextPage")
-      }
-    })
-    
-    observeEvent(input$prevPage, {
-      current_page(max(1, current_page() - 1))
-    })
-    
-    observeEvent(input$nextPage, {
-      current_page(min(num_pages, current_page() + 1))
-    })
-    
-    output$downloadPdf <- downloadHandler(
-      filename = function() {
-        "plots.pdf"
-      },
-      content = function(file) {
-        pdf(file = file, width = 12, height = 14)
-        for (plot in plots) {
-          print(plot)
+      # Generate the plots
+      plots <- lapply(seq_along(curves), function(i) {
+        curve_data <- data.frame(
+          t = seq(0, curves[[i]]$basis$rangeval[2]),
+          x = eval.fd(seq(0, curves[[i]]$basis$rangeval[2]), curves[[i]], Lfdobj = 0)
+        )
+        names(curve_data) <- c("t","x")
+        
+        motif_lines <- mapply(function(id_motif, pos_motif, instance) {
+          motif_t = seq((pos_motif - 1) * builder@dist_knots,
+                        (pos_motif - 1 + length(curves$coeff_motifs[[id_motif]]) - builder@norder + 1) * builder@dist_knots)
+          motif_x = eval.fd(motif_t, curves[[i]], Lfdobj = 0)
+          data.frame(t = motif_t, x = motif_x, motif_id = factor(paste(id_motif, instance, sep = "_")),
+                     initial_number = str_extract(as.character(id_motif), "^[^_]+"),
+                     xmin = (pos_motif - 1) * builder@dist_knots,
+                     xmax = (pos_motif - 1 + length(curves$coeff_motifs[[id_motif]]) - builder@norder + 1) * builder@dist_knots)
+        }, builder@motifs_in_curves[[i]]$motif_id, builder@motifs_in_curves[[i]]$starting_coeff_pos, seq_along(builder@motifs_in_curves[[i]]$motif_id), SIMPLIFY = FALSE)
+        
+        motif_data <- bind_rows(motif_lines)
+        names(motif_data) <- c("t","x","motif_id","initial_number","xmin","xmax")
+        
+        motif_colors <- c( "1" = "red", "2" = "green", "3" = "blue", "4" = "orange",
+                           "5" = "purple", "6" = "cyan", "7" = "magenta", "8" = "brown",
+                           "9" = "pink", "10" = "grey")
+        motif_colors <- rep(motif_colors,length.out = length(mot_details))
+        if(length(mot_details) > 10 )
+          attr(motif_colors,"names")[11:length(mot_details)] <- as.character(as.integer(attr(motif_colors,"names")[11:length(mot_details)]) + 10)
+        
+        if (nrow(motif_data) > 0) {
+          p <- ggplot() +
+            # Plot the main curve in black
+            geom_line(data = curve_data, aes(x = t, y = x), color = "black", size = 0.5) +
+            # Add shaded rectangles for motif positions with transparency
+            geom_rect(data = motif_data, aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf, fill = initial_number), alpha = 0.01) +
+            # Plot motifs with distinct colors
+            geom_line(data = motif_data, aes(x = t, y = x, color = factor(initial_number), group = motif_id), size = 1.5) + 
+            scale_color_manual(values = motif_colors) +
+            scale_fill_manual(values = motif_colors) +
+            labs(title = paste('Random curve', i), x = 't', y = 'x(t)') +
+            theme_minimal(base_size = 15) +
+            ylim(-20, 20) +
+            guides(color = guide_legend(title = "Motif ID"), fill = guide_legend(title = "Motif ID"))
         }
-        dev.off()
-      }
-    )
+      })
+      plots <- Filter(function(plot){!is.null(plot)},plots)
+      # Pagination settings
+      plots_per_page <- 2
+      num_pages <- ceiling(length(plots) / plots_per_page)
+      current_page <- reactiveVal(1)
+      
+      output$plots_ui <- renderUI({
+        page <- current_page()
+        start <- (page - 1) * plots_per_page + 1
+        end <- min(page * plots_per_page, length(plots))
+        
+        plot_output_list <- lapply(start:end, function(i) {
+          plotname <- paste0("plot", i)
+          fluidRow(
+            column(
+              width = 12,
+              div(class = "plot-container",
+                  div(class = "plot-title"),
+                  plotOutput(outputId = plotname, height = "350px", width = "150%")
+              )
+            )
+          )
+        })
+        
+        do.call(tagList, plot_output_list)
+      })
+      
+      lapply(seq_along(plots), function(i) {
+        output[[paste0("plot", i)]] <- renderPlot({
+          plots[[i]]
+        })
+      })
+      
+      observe({
+        shinyjs::enable("prevPage")
+        shinyjs::enable("nextPage")
+        
+        if (current_page() == 1) {
+          shinyjs::disable("prevPage")
+        }
+        
+        if (current_page() == num_pages) {
+          shinyjs::disable("nextPage")
+        }
+      })
+      
+      observeEvent(input$prevPage, {
+        current_page(max(1, current_page() - 1))
+      })
+      
+      observeEvent(input$nextPage, {
+        current_page(min(num_pages, current_page() + 1))
+      })
+      
+      output$downloadPdf <- downloadHandler(
+        filename = function() {
+          "plots.pdf"
+        },
+        content = function(file) {
+          pdf(file = file, width = 12, height = 14)
+          for (plot in plots) {
+            print(plot)
+          }
+          dev.off()
+        }
+      )
+      
+      # Clear any previous error message
+      shinyjs::html("error_message", "")
+      
+    }, error = function(e) {
+      # Display error message in UI
+      shinyjs::html("error_message", paste("Error: ", e$message))
+    }, finally = {
+      # Hide progress bar
+      remove_modal_spinner()
+    })
   })
 }
 
