63ab165e2e4e20d758bc668dfe6a85605e94ca9a
NiccoloF <niccolo.feresini@mail.polimi.it>
2024-08-04 19:32:38 +0200

    Added shiny app and motif simulation

---
+++ mtfd/R/S4_constructor.R
@@ -0,0 +1,245 @@
+#' @title Create MyS4Class Object
+#' @description A constructor function for the S4 class MyS4Class.
+#' @param name A character string representing the name.
+#' @param value A numeric value.
+#' @return An object of class MyS4Class.
+#' @export
+motifSimulationBuilder <- function(curve_details = list(N=20,
+                                                        len = 300,
+                                                        norder = 3,
+                                                        coeff_min=-15,
+                                                        coeff_max=15,
+                                                        dist_knots=10,
+                                                        min_dist_motifs=norder * dist_knots),
+                                   mot_details,
+                                   distribution = 'unif') {
+  ########################## UNPACKING #################################
+  N <- NULL
+  len <- NULL
+  norder <- NULL
+  coeff_min <- NULL
+  coeff_max <- NULL
+  dist_knots <- NULL
+  min_dist_motifs <- NULL
+  
+  if('N' %in% names(curve_details)) {
+    N <- curve_details[['N']]
+  }else{
+    N <- curve_details[[1]]
+  }
+  if('len' %in% names(curve_details)) {
+    len <- curve_details[['len']]
+  }else{
+    len <- curve_details[[2]]
+  }
+  if('norder' %in% names(curve_details)) {
+    norder <- curve_details[['norder']]
+  }else{
+    norder <- curve_details[[3]]
+  }
+  if('coeff_min' %in% names(curve_details)) {
+    coeff_min <- curve_details[['coeff_min']]
+  }else{
+    coeff_min <- curve_details[[4]]
+  }
+  if('coeff_max' %in% names(curve_details)) {
+    coeff_max <- curve_details[['coeff_max']]
+  }else{
+    coeff_max <- curve_details[[5]]
+  }
+  if('dist_knots' %in% names(curve_details)) {
+    dist_knots <- curve_details[['dist_knots']]
+  }else{
+    dist_knots <- curve_details[[6]]
+  }
+  if('min_dist_motifs' %in% names(curve_details)) {
+    min_dist_motifs <- curve_details[['min_dist_motifs']]
+  }else{
+    min_dist_motifs <- curve_details[[7]]
+  }
+  
+  ########################## CHECKS #################################
+  # check N, dist_knots and len
+  if((N%%1!=0)|(N<1))
+    stop('Invalid \'N\'.')
+  if((dist_knots%%1!=0)|(dist_knots<1))
+    stop('Invalid \'dist_knots\'.')
+  if(TRUE %in% ((len%%1!=0)|(len<1)|(sum(len%%dist_knots)>0)))
+    stop('Invalid \'len\'.')
+  if(length(mot_details) == 0)
+    stop('Invalid \'mot_details\'.')
+  
+  weights_defined <- sapply(mot_details, function(x) !is.null(x$weights))
+  if (!(all(weights_defined) || all(!weights_defined))) {
+    stop("Inconsistent weights field: some elements have it defined, others do not.")
+  }
+  
+  len_motifs <- sapply(mot_details,function(x){x$len},simplify = TRUE)
+  nmotifs <- length(mot_details)
+  if(TRUE %in% weights_defined) {
+    for(i in 1:nmotifs){
+      nbasis <- len_motifs[i]/dist_knots + norder - 1
+      mot_details[[i]]$weights <- mot_details[[i]]$weights[1:nbasis]
+      
+    }
+  }
+  # check nmotifs and len_motifs
+  if((nmotifs%%1!=0)|(nmotifs<1))
+    stop('Invalid \'nmotifs\'.')
+  if(TRUE %in% ((len_motifs%%1!=0)|(len_motifs<1)|(sum(len_motifs%%dist_knots)>0)|(TRUE %in% (len_motifs>len))))
+    stop('Invalid \'len_motifs\'.')
+  
+  if (FALSE %in% unlist(lapply(mot_details, function(x) {
+    if (all(weights_defined)) {
+      x$len == (length(x$weights)-norder+1)*dist_knots
+    } else {
+      TRUE
+    }
+  }))) {
+    stop("The length of the motifs is incompatible ")
+  }
+  # check min_dist_motifs
+  if((min_dist_motifs < norder*dist_knots)|(min_dist_motifs%%dist_knots>0))
+    stop('Invalid \'min_dist_motifs\'.')
+  # check freq_motifs and convert it into a list with the frequencies for the different motifs in the different curves
+  
+  freq_motifs_vector <- sapply(mot_details,function(mot_i){ifelse(is.data.frame(mot_i$appearance),dim(mot_i$appearance['curve'])[1],NA)})
+  is_appearance_defined <- !(NA %in% freq_motifs_vector)
+  if(sum(is.na(freq_motifs_vector)) != 0 && sum(is.na(freq_motifs_vector)) != nmotifs) {
+    stop("Inconsistent appearance field: some elements have it defined, others do not.")
+  }
+  if(is_appearance_defined) {
+    freq_motifs_vector <- freq_motifs_vector[!is.na(freq_motifs_vector)]
+    if(TRUE %in% ((freq_motifs_vector%%1!=0)|(freq_motifs_vector<1)|(sum(norder-1+rep(len/dist_knots,length.out=N)-2*(norder-1))<(sum(rep(len_motifs/dist_knots+norder-1,length.out=nmotifs)*rep(freq_motifs_vector,length.out=nmotifs))+max(0,sum(rep(freq_motifs_vector, length.out = nmotifs))-N)*(min_dist_motifs/dist_knots-norder+1)))))
+      stop('Invalid \'freq_motifs\'.')
+    motif_str_list <- lapply(mot_details, function(x) {
+      df <- x$appearance
+      len <- x$len
+      df$len <- len  # Add the length to each motif dataframe
+      return(df)
+    })
+    motif_str_new <- do.call(rbind, motif_str_list)
+    
+    # Convert columns to numeric if necessary
+    motif_str_new[] <- lapply(motif_str_new, as.numeric)
+    
+    # Add columns for start and end positions
+    motif_str_new <- motif_str_new %>%
+      mutate(
+        start = (start_break_pos - 1) * dist_knots,
+        end = start + len
+      )
+    # Apply the check for each curve
+    valid_curves <- mapply(function(sub_data) {
+                            mtfd:::.check_fits(sub_data, min_dist_motifs = min_dist_motifs)
+                          },
+                          split(motif_str_new, motif_str_new$curve), 
+                          SIMPLIFY = TRUE)
+    
+    # Check if all curves have valid motifs
+    if(FALSE %in% valid_curves) {
+      stop('Invalid position of the knots.')
+    }
+  }
+  
+  
+  freq_check=1
+  it=0
+  
+  # Compute the frequency(if not defined) of the motifs inside the curves 
+  if(!is_appearance_defined) {
+    while((sum(freq_check)>0)&&(it<10000)){
+      it=it+1
+      freq_motifs <- matrix(data = 0,nrow = N,ncol = nmotifs)
+      for(motif_j in 1:nmotifs) {
+        curves <- as.vector(sample(N,mot_details[[motif_j]]$appearance,replace=TRUE))
+        for (curve in curves) {
+          freq_motifs[curve, motif_j] <- freq_motifs[curve, motif_j] + 1
+        }
+      }
+      freq_motifs=split(freq_motifs,rep(1:N,nmotifs))# construct N list(for each curve). Each list_j contains how many times the motif_i appears in the curve j
+      freq_check=mapply(function(freq_motifs_i,len_i) ((len_i-2*(norder-1))<(sum(rep(len_motifs/dist_knots+norder-1,length.out=nmotifs)*rep(freq_motifs_i,length.out=nmotifs))+(sum(freq_motifs_i)-1)*(min_dist_motifs/dist_knots-norder+1))),
+                        freq_motifs,norder-1+rep(len/dist_knots,length.out=N))
+    }
+    if(it==10000)
+      stop('Tried 10.000 random configurations of motif frequencies in the different curves, unable to find a valid configuration. Please select lower \'freq_motifs\'.')
+  }else {
+    freq_motifs <- matrix(data = 0,nrow = N,ncol = nmotifs)
+    for(motif_j in 1:nmotifs) {
+      for(curve_i in 1:N){
+        count <- sum(mot_details[[motif_j]]$appearance$curve == curve_i)
+        freq_motifs[curve_i, motif_j] <-  freq_motifs[curve_i, motif_j] + count
+      }
+    }
+    ifelse(TRUE %in% mapply(function(freq_motifs_i,len_i) ((len_i-2*(norder-1))<(sum(rep(len_motifs/dist_knots+norder-1,length.out=nmotifs)*rep(freq_motifs_i,length.out=nmotifs))+(sum(freq_motifs_i)-1)*(min_dist_motifs/dist_knots-norder+1))),
+                            freq_motifs,norder-1+rep(len/dist_knots,length.out=N)),"",stop('Please select lower \'freq_motifs\'.'))
+  }
+  
+  ########################## POSITION ASSIGNMENT ###############################
+  # randomly assign motif positions in curves if they are not already assigned
+  # At this point we only have the coefficients of the motifs
+  motifs_in_curves <- vector("list", N)
+  if (!is_appearance_defined) {
+    # Initialize appearance for each motif
+    for (mot in 1:nmotifs) {
+      mot_details[[mot]]$appearance <- data.frame(motif_id = integer(0),
+                                                  curve = integer(0),
+                                                  start_break_pos = integer(0),
+                                                  coeff_pos = integer(0))
+    }
+    
+    motifs_in_curves <- mapply(function(freq_motifs_i, len_i, curve_i) {
+      if (sum(freq_motifs_i) == 0) {
+        return(NULL) # No motifs embedded in this curve
+      }
+      
+      id_motifs <- mtfd:::.resample(rep(seq_along(freq_motifs_i), freq_motifs_i))
+      len_elements <- c(norder - 1, rep(len_motifs / dist_knots + norder - 1, length.out = nmotifs)[id_motifs] + c(rep((min_dist_motifs / dist_knots - norder + 1), sum(freq_motifs_i) - 1), 0), norder - 1)
+      gaps_tot <- len_i - sum(len_elements) # number of free coefficients
+      gaps <- diff(c(0, sort(sample(gaps_tot + sum(freq_motifs_i), sum(freq_motifs_i))))) - 1
+      coeff_pos <- cumsum(len_elements[seq_along(id_motifs)]) + 1 + cumsum(gaps) # Starting coefficient
+      
+      for (mot in unique(id_motifs)) {
+        indices <- which(id_motifs == mot)
+        df <- data.frame(
+          motif_id = mot,
+          curve = rep(curve_i, length(indices)),
+          start_break_pos = coeff_pos[indices] # coincide with the coeffs position
+        )
+        mot_details[[mot]]$appearance <<- rbind(mot_details[[mot]]$appearance, df)
+      }
+      
+      return(list(motif_id=id_motifs,starting_coeff_pos=coeff_pos)) # returns N list(one for each curve) with the id of the motif embedded and the starting position
+    }, freq_motifs, norder - 1 + rep(len / dist_knots, length.out = N), 1:N,SIMPLIFY = FALSE)
+  } else {
+    splited_curves <- split(motif_str_new, motif_str_new$curve)
+    names(motifs_in_curves) <- as.character(1:N)
+    for (name in as.character(1:N)) {
+      if (name %in% names(splited_curves)) {
+        motifs_in_curves[[name]] <- list(motif_id = splited_curves[[name]]$motif_id,starting_coeff_pos = splited_curves[[name]]$start_break_pos)
+      }
+    }
+  }
+  
+  ########################## WEIGHTS GENERATION #################################
+  # if weights are not provided, randomly generate them according to the distribution
+  if(all(!weights_defined)) {
+    mot_details <- mapply(mtfd:::.generate_coefficients,mot_details,
+                          MoreArgs = list(distrib = distribution,
+                                          dist_knots = dist_knots,
+                                          norder = norder, 
+                                          coeff_min = coeff_min,
+                                          coeff_max = coeff_max),
+                          SIMPLIFY = FALSE)
+  }
+  
+  ########################## RETURN #################################
+  return(new("motifSimulation",N = N,
+             mot_details = mot_details,
+             motifs_in_curves = motifs_in_curves,
+             distribution = distribution,
+             dist_knots=dist_knots,len=len,norder=norder,
+             coeff_min=coeff_min,coeff_max=coeff_max,
+             min_dist_motifs=min_dist_motifs,
+             is_appearance_defined = is_appearance_defined))
+}
---
+++ mtfd/R/shiny.R
@@ -0,0 +1,412 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(shinyjs)
+library(shinyWidgets)
+library(shinybusy)
+library(data.table)
+
+ui <- fluidPage(
+  useShinyjs(),  # Initialize shinyjs
+  useSweetAlert(),  # Initialize shinyWidgets
+  
+  tags$head(
+    tags$style(HTML("
+      /* Your custom CSS */
+      body {
+        background-color: #001f3f;
+        color: #ffffff;
+        font-family: 'Arial', sans-serif;
+      }
+      .well {
+        background-color: #003366;
+        color: white;
+        border: none;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
+        padding: 20px;
+      }
+      .btn-primary {
+        background-color: #007bff;
+        border: none;
+        padding: 6px 12px;  /* Increased padding for larger clickable area */
+        font-size: 18px;     /* Larger font size */
+        cursor: pointer;
+        border-radius: 8px;  /* Rounded corners */
+        transition: background-color 0.3s, transform 0.2s; /* Smooth transitions */
+      }
+      .btn-primary:hover {
+        background-color: #0056b3;
+        transform: scale(1.05);  /* Slightly increase button size on hover */
+      }
+      .btn-primary:active {
+        background-color: #004080;
+        transform: scale(1.02);  /* Slightly reduce button size on click */
+      }
+      #pagination_controls {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+      }
+      .pagination-buttons {
+        margin: 0 10px;
+        background-color: #007bff;
+        border: none;
+        color: white;
+        padding: 6px 12px;  /* Increased padding for larger clickable area */
+        font-size: 18px;     /* Larger font size */
+        border-radius: 8px;  /* Rounded corners */
+        cursor: pointer;
+        transition: background-color 0.3s, transform 0.2s; /* Smooth transitions */
+      }
+      .pagination-buttons:disabled {
+        background-color: #cccccc;
+        cursor: not-allowed;
+      }
+      .pagination-buttons:hover:not(:disabled) {
+        background-color: #0056b3;
+        transform: scale(1.05);  /* Slightly increase button size on hover */
+      }
+      .pagination-buttons:active:not(:disabled) {
+        background-color: #004080;
+        transform: scale(1.02);  /* Slightly reduce button size on click */
+      }
+      .collapsible {
+        cursor: pointer;
+        padding: 12px;
+        background-color: #003366;
+        border: none;
+        color: white;
+        text-align: left;
+        outline: none;
+        font-size: 18px;  /* Larger font size */
+        width: 100%;
+        display: block;
+        margin-bottom: 5px;
+        border-radius: 8px;  /* Rounded corners */
+        transition: background-color 0.3s; /* Smooth transitions */
+      }
+      .active, .collapsible:hover {
+        background-color: #00509e;
+      }
+      .content {
+        display: none;
+        padding: 10px;
+        background-color: #002244;
+        border-radius: 5px;
+        margin-bottom: 10px;
+      }
+      .plot-container {
+        margin-bottom: 20px;
+      }
+      .plot-title {
+        text-align: center;
+        margin-bottom: 10px;
+      }
+      .input-row {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+      }
+      .input-group {
+        flex: 1;
+        margin-right: 10px;
+      }
+      .input-group:last-child {
+        margin-right: 0;
+      }
+      #error_message {
+        color: red;
+        font-weight: bold;
+        margin-top: 20px;
+      }
+    ")),
+    tags$script(HTML("
+      $(document).on('click', '.collapsible', function() {
+        $(this).toggleClass('active');
+        $(this).next('.content').slideToggle('fast');
+      });
+    "))
+  ),
+  
+  titlePanel("Motif Simulation Plotter"),
+  
+  mainPanel(
+    uiOutput("plots_ui"),
+    fluidRow(
+      column(width = 12, 
+             actionButton("plotBtn", "Generate Plots", class = "btn btn-primary"),
+             tags$div(
+               id = "pagination_controls",
+               actionButton("prevPage", "Previous", class = "pagination-buttons", disabled = TRUE),
+               actionButton("nextPage", "Next", class = "pagination-buttons", disabled = TRUE)
+             )
+      )
+    ),
+    downloadButton("downloadPdf", "Download PDF"),
+    div(id = "error_message", "")
+  ),
+  
+  fluidRow(
+    column(width = 12,
+           wellPanel(
+             div(class = "input-row",
+                 div(class = "input-group", numericInput("N", "Number of Curves (N)", value = 20, min = 1)),
+                 div(class = "input-group", numericInput("len", "Curve Length (len)", value = 300, min = 1))
+             ),
+             div(class = "input-row",
+                 div(class = "input-group", numericInput("norder", "Order of Basis (norder)", value = 3, min = 1)),
+                 div(class = "input-group", numericInput("coeff_min", "Minimum Coefficient (coeff_min)", value = -15))
+             ),
+             div(class = "input-row",
+                 div(class = "input-group", numericInput("coeff_max", "Maximum Coefficient (coeff_max)", value = 15)),
+                 div(class = "input-group", numericInput("dist_knots", "Distance Between Knots (dist_knots)", value = 10, min = 1))
+             ),
+             div(class = "input-row",
+                 div(class = "input-group", numericInput("min_dist_motifs", "Minimum Distance Between Motifs (min_dist_motifs)", value = 30, min = 1)),
+                 div(class = "input-group", selectInput("distribution", "Coefficient Distribution", choices = c("unif", "beta"), selected = "unif"))
+             ),
+             fileInput("file2", "Choose CSV File for error structure", accept = c(".csv")),
+             radioButtons("inputMethod", "Input Method", choices = c("Set Number of Motifs" = "manual", "Upload CSV" = "file"), selected = "manual"),
+             conditionalPanel(
+               condition = "input.inputMethod == 'manual'",
+               numericInput("numMotifs", "Number of Motifs", value = 2, min = 1),
+               uiOutput("motifInputs")
+             ),
+             conditionalPanel(
+               condition = "input.inputMethod == 'file'",
+               fileInput("file1", "Choose CSV File for motif's appearance", accept = c(".csv"))
+             ),
+             textInput("path", "Output Directory", value = tempdir())
+           )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  observeEvent(input$numMotifs, {
+    output$motifInputs <- renderUI({
+      numMotifs <- input$numMotifs
+      lapply(1:numMotifs, function(i) {
+        tagList(
+          actionButton(paste0("toggleMotif", i), paste("Motif", i), class = "collapsible"),
+          div(id = paste0("motif", i, "Content"), class = "content",
+              numericInput(paste0("mot", i, "_len"), paste("Motif", i, "Length"), value = 100, min = 1),
+              textInput(paste0("mot", i, "_weights"), paste("Motif", i, "Weights (comma-separated)"), value = ""),
+              numericInput(paste0("mot", i, "_appearance"), paste("Motif", i, "Appearance"), value = 10, min = 0)
+          )
+        )
+      })
+    })
+  })
+  
+  observeEvent(input$plotBtn, {
+    req(input$path)
+    
+    # Show progress bar
+    show_modal_spinner(spin = "circle", text = "Generating plots, please wait...")
+    
+    tryCatch({
+      curve_details <- list(
+        N = input$N,
+        len = input$len,
+        norder = input$norder,
+        coeff_min = input$coeff_min,
+        coeff_max = input$coeff_max,
+        dist_knots = input$dist_knots,
+        min_dist_motifs = input$min_dist_motifs
+      )
+      
+      parse_weights <- function(weights_input) {
+        if (weights_input == "") {
+          return(NULL)
+        }
+        as.numeric(unlist(strsplit(weights_input, ",")))
+      }
+      
+      if (input$inputMethod == "manual") {
+        mot_details <- lapply(1:input$numMotifs, function(i) {
+          list(
+            len = input[[paste0("mot", i, "_len")]],
+            weights = parse_weights(input[[paste0("mot", i, "_weights")]]),
+            appearance = input[[paste0("mot", i, "_appearance")]]
+          )
+        })
+      } else {
+        req(input$file1)
+        req(input$file2)
+        dt <- fread(input$file1$datapath)
+        if (ncol(dt) != 3) {
+          stop("CSV file must have exactly 3 columns.")
+        }
+        setnames(dt, c("motif_id", "curve", "start_break_pos"))
+        unique_motif_ids <- unique(dt$motif_id)
+        mot_details <- lapply(1:length(unique_motif_ids), function(i) {
+          list(
+            len = input[[paste0("mot", i, "_len")]],
+            weights = parse_weights(input[[paste0("mot", i, "_weights")]]),
+            appearance = as.data.frame(dt[motif_id == unique_motif_ids[i]])
+          )
+        })
+      }
+      error_str <- fread(input$file2$datapath)
+      if(dim(error_str)[1] == 1) {
+        error_str <- as.vector(error_str)
+      }else {
+        error_str <- as.matrix(error_str)
+      }
+      distribution <- input$distribution
+      if (!dir.exists(input$path)) {
+        stop("Invalid directory path.")
+      }
+
+      builder <- mtfd::motifSimulationBuilder(curve_details, mot_details, distribution)
+      curves <- mtfd::generateCurves(builder,error_str)
+      
+      output_file <- file.path(input$path, "plots.pdf")
+      
+      if (!dir.exists(input$path)) {
+        dir.create(input$path)
+      }
+      plots <- lapply(seq_along(curves),function(k){
+          if(!is.null(curves[[k]]$with_error$error_y)) {
+            curve_data_no_error <- data.frame(
+              t = seq(0, curves[[k]]$no_error$basis$rangeval[2]-1),
+              x = curves[[k]]$no_error$no_error_y)
+            names(curve_data_no_error) <- c("t","x")
+            
+            curve_data_error <- NULL
+            curve_data_error <- data.frame(
+              t = seq(0, curves[[k]]$no_error$basis$rangeval[2]-1),
+              x = curves[[k]]$with_error$error_y)
+            names(curve_data_error) <- c("t",paste0("x",seq(length(curves[[k]]$with_error$error_y))))
+            
+            motif_lines <- mapply(function(id_motif, pos_motif, instance) {
+              motif_t = seq((pos_motif - 1) * builder@dist_knots,
+                            (pos_motif - 1) * builder@dist_knots + builder@mot_details[[id_motif]]$len)
+              motif_x = lapply(curves[[k]]$with_error$error_y,function(curve){return(curve[motif_t + 1])})
+              
+              return(lapply(motif_x,function(motif){ data.frame(t = motif_t, x = motif, motif_id = factor(paste(id_motif, instance, sep = "_")),
+                                                                initial_number = str_extract(as.character(id_motif), "^[^_]+"),
+                                                                xmin = (pos_motif - 1) * builder@dist_knots,
+                                                                xmax = (pos_motif - 1) * builder@dist_knots + builder@mot_details[[id_motif]]$len)}))
+            }, builder@motifs_in_curves[[k]]$motif_id, builder@motifs_in_curves[[k]]$starting_coeff_pos, seq_along(builder@motifs_in_curves[[k]]$motif_id), SIMPLIFY = FALSE)
+            
+            motif_colors <- c( "1" = "red", "2" = "blue", "3" = "darkgreen", "4" = "orange",
+                               "5" = "purple", "6" = "cyan", "7" = "magenta", "8" = "brown",
+                               "9" = "pink", "10" = "grey")
+            motif_colors <- rep(motif_colors,length.out = length(builder@mot_details))
+            if(length(builder@mot_details) > 10 )
+              attr(motif_colors,"names")[11:length(builder@mot_details)] <- as.character(as.integer(attr(motif_colors,"names")[11:length(builder@mot_details)]) + 10)
+            
+            max_dataframes <- max(sapply(motif_lines, function(sublist) length(sublist)))
+            # Inizializza una lista per memorizzare i risultati
+            motif_data <- vector("list", max_dataframes)
+            # Cicla su ogni "livello" dei data frame
+            for (i in seq_len(max_dataframes)) {
+              # Estrai i data frame dal livello i-esimo
+              dataframes_at_level_i <- lapply(motif_lines, function(sublist) {
+                sublist[[i]]
+              })
+              # Fai il bind_rows sui data frame estratti
+              motif_data[[i]] <- bind_rows(dataframes_at_level_i)
+              names(motif_data[[i]]) <- c("t","x","motif_id","initial_number","xmin","xmax")
+            }
+            return(lapply(1:length(motif_data),function(j) {
+              pic <- ggplot() +
+                # Plot the main curve in black
+                geom_line(data = curve_data_no_error, aes(x = t, y = x), color = scales::alpha('gray30',0.15), linewidth = 0.5) + 
+                # Plot the error curve in black
+                geom_line(data = curve_data_error, aes_string(x = "t", y = paste0("x", j)), color = "black", linewidth = 0.5) +
+                # Add shaded rectangles for motif positions with transparency
+                geom_rect(data = motif_data[[j]], aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf, fill = initial_number), alpha = 0.005) +
+                # Plot motifs with distinct colors
+                geom_line(data = motif_data[[j]], aes(x = t, y = x, color = factor(initial_number), group = motif_id), linewidth = 1.0) + 
+                scale_color_manual(values = motif_colors) +
+                scale_fill_manual(values = motif_colors) +
+                labs(title = paste('Random curve', k), aes_string(x = "t", y = paste0("x", j))) +
+                theme_minimal(base_size = 15) +
+                guides(color = guide_legend(title = "Motif ID"), fill = guide_legend(title = "Motif ID"))
+              return(pic)
+            }))
+          }
+      })
+      plots <- unlist(plots,recursive = FALSE)
+      
+      plots_per_page <- 2
+      num_pages <- ceiling(length(plots) / plots_per_page)
+      current_page <- reactiveVal(1)
+      
+      output$plots_ui <- renderUI({
+        page <- current_page()
+        start <- (page - 1) * plots_per_page + 1
+        end <- min(page * plots_per_page, length(plots))
+        
+        plot_output_list <- lapply(start:end, function(i) {
+          plotname <- paste0("plot", i)
+          fluidRow(
+            column(
+              width = 12,
+              div(class = "plot-container",
+                  div(class = "plot-title"),
+                  plotOutput(outputId = plotname, height = "350px", width = "150%")
+              )
+            )
+          )
+        })
+        
+        do.call(tagList, plot_output_list)
+      })
+      
+      lapply(seq_along(plots), function(i) {
+        output[[paste0("plot", i)]] <- renderPlot({
+          plots[[i]]
+        })
+      })
+      
+      observe({
+        shinyjs::enable("prevPage")
+        shinyjs::enable("nextPage")
+        
+        if (current_page() == 1) {
+          shinyjs::disable("prevPage")
+        }
+        
+        if (current_page() == num_pages) {
+          shinyjs::disable("nextPage")
+        }
+      })
+      
+      observeEvent(input$prevPage, {
+        current_page(max(1, current_page() - 1))
+      })
+      
+      observeEvent(input$nextPage, {
+        current_page(min(num_pages, current_page() + 1))
+      })
+      
+      output$downloadPdf <- downloadHandler(
+        filename = function() {
+          "plots.pdf"
+        },
+        content = function(file) {
+          pdf(file = file, width = 12, height = 14)
+          for (plot in plots) {
+            print(plot)
+          }
+          dev.off()
+        }
+      )
+      
+      shinyjs::html("error_message", "")
+      
+    }, error = function(e) {
+      shinyjs::html("error_message", paste("Error: ", e$message))
+    }, finally = {
+      remove_modal_spinner()
+    })
+  })
+}
+
+shinyApp(ui = ui, server = server)
